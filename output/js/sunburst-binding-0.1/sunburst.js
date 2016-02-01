288f148c62715afa492c97e90b8cc5f2573331d4
Duc-Quang Nguyen <ducquang.nguyen@gmail.com>
2016-02-01 15:21:48 +0100

    frist studio draft QW

--- output/js/sunburst-binding-0.1/sunburst.js
+++ output/js/sunburst-binding-0.1/sunburst.js
@@ -191,7 +191,7 @@ HTMLWidgets.widget({
       // Transition each segment to full opacity and then reactivate it.
       d3.select(el).selectAll("path")
           .transition()
-          .duration(1000)
+          .duration(400)
           .style("opacity", 1)
           .each("end", function() {
                   d3.select(this).on("mouseover", mouseover);
